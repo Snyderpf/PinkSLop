8175fbf79b323ec13e446a1de8e8c5f2d659ce18
Snyderpf <petitsnyder@gmail.com>
2025-12-12 15:32:36 -0500

    remove item from view

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,7 @@ cart.forEach((cartItem) => {
 
     cartSummaryHTML += 
     `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -111,5 +111,10 @@ document.querySelectorAll('.js-delete-link')
             removeFromCart(productID);
             console.log(cart);
 
-        } )
+        
+        const container  = document.querySelector(
+            `.js-cart-item-container-${productID}`
+        );
+            container.remove();
+        });
     });
