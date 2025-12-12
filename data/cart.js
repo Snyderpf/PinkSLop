982b7238c9043686ca1bf34b0e257a1734fe3e11
Snyderpf <petitsnyder@gmail.com>
2025-12-12 15:50:06 -0500

    loading cart from localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,24 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
-    productId :"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity : 2,
-},
+if (!cart) {
+
+    cart = [{
+
+        productId :"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity : 2,
+    },
+    {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d", 
+        quantity:  3
+    }];
+
+}
+
+
+function saveToStorage()
 {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d", 
-    quantity:  3
-}];
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 
 export function addToCart(productId)
@@ -27,7 +39,11 @@ export function addToCart(productId)
                 quantity: 1
             });
         }
+        
+        saveToStorage();
 }
+
+
 
 export function removeFromCart(productId){
     const newCart = [];
@@ -40,5 +56,7 @@ export function removeFromCart(productId){
         }
     });
     
-    cart = newCart; 
+    cart = newCart;
+    
+    saveToStorage();
 }
