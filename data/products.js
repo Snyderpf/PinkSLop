088096e7e0374e7cb4965c37b13ee4ddc5a6c169
Snyderpf <petitsnyder@gmail.com>
2025-12-16 17:19:11 -0500

    remove product 1 examp

--- data/products.js
+++ data/products.js
@@ -35,17 +35,7 @@ class Product {
   }
 }
 
-const product1 = new Product({
-  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-  rating: {
-    stars: 4.5,
-    count: 87,
-  },
-  priceCents: 1090,
-  keywords: ["socks", "sports", "apparel"],
-});
+
 
 export const products = [
   {
@@ -522,4 +512,4 @@ export const products = [
   return new Product(productDetails);
 });
 
-// console.log(products);
+
