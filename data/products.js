2e856d4fe3a6aafed40546f31fc08d5020741bef
Snyderpf <petitsnyder@gmail.com>
2025-12-17 13:11:21 -0500

    created clothing class(using inheritance and polymorphism for size chart)

--- data/products.js
+++ data/products.js
@@ -33,9 +33,29 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return "";
+  }
 }
 
+class Clothing extends Product {
+  sizeChartLink;
 
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    return `
+      <a href="${this.sizeChartLink}" target = "_blank"> Size chart </a>
+    `;
+  }
+}
+
+console.log(tshirt);
+console.log(tshirt.getPrice);
 
 export const products = [
   {
@@ -509,7 +529,8 @@ export const products = [
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
 ].map((productDetails) => {
+  if (productDetails.type === "clothing") {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
-
-
